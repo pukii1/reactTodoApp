6bbc22ed2b4c706674039a6729abbaa417e8e9ee
Pukii <salima.pusch@gmx.de>
2023-05-13 16:22:19 +0200

    properly aligned the notes TODO disable note deletion when parent task is checked off

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTrash} from '@fortawesome/free-solid-svg-icons';
+import { faTimes} from '@fortawesome/free-solid-svg-icons';
 
 export default function Note({deleteNote,note, idx, taskIdx}) {
   return (
-    <div>
-      <p className="taskNote">{note}</p>
-      <button className="btnDelete" onClick={()=> {deleteNote(idx, taskIdx)}}>
-                  <FontAwesomeIcon className="icon" icon={faTrash} />
+    <div className="noteContainer">
+      <p className="taskNote">
+        {note}
+      </p>
+      <button className="btnDeleteNote inline" onClick={()=> {deleteNote(idx, taskIdx)}}>
+                  <FontAwesomeIcon className="icon" icon={faTimes} />
         </button>
     </div>
   )
